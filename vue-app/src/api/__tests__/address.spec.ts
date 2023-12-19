af869ca648812d131c99400242235f620576598b
yuetloo <contact@yuetloo.com>
2023-12-19 14:40:40 -0500

    need eslint config back in workspace root

--- vue-app/src/api/__tests__/address.spec.ts
+++ vue-app/src/api/__tests__/address.spec.ts
@@ -4,3 +4,31 @@ it('same addresses', () => {
   const address = '0x78a3bce2b6E6132Fe773E1379DCcEdBd8911FE59'
   expect(isAddressEqual(address, address)).to.eq(true)
 })
+
+it('different addresses', () => {
+  const address = '0x78a3bce2b6E6132Fe773E1379DCcEdBd8911FE59'
+  const address2 = '0x28a3bce2b6E6132Fe773E1379DCcEdBd8911FE59'
+  expect(isAddressEqual(address, address2)).to.eq(false)
+})
+
+it('1 address is undefined', () => {
+  const address = '0x78a3bce2b6E6132Fe773E1379DCcEdBd8911FE59'
+  const address2 = undefined
+  expect(isAddressEqual(address, address2)).to.eq(false)
+})
+
+it('both addresses are undefined', () => {
+  const address2 = undefined
+  expect(isAddressEqual(address2, address2)).to.eq(false)
+})
+
+it('both addresses are empty strings', () => {
+  const address2 = ''
+  expect(isAddressEqual(address2, address2)).to.eq(false)
+})
+
+it('1 address is empty strings', () => {
+  const address = '0x78a3bce2b6E6132Fe773E1379DCcEdBd8911FE59'
+  const address2 = ''
+  expect(isAddressEqual(address, address2)).to.eq(false)
+})
