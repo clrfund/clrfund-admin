ae13ea764ac26013b8e9dedb7f2c9d230afbfdc6
yuetloo <contact@yuetloo.com>
2024-06-26 19:50:37 -0400

    do not load clrfund data on app

--- vue-app/app.vue
+++ vue-app/app.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script setup>
-const route = useRoute()
 const app = useAppStore()
 const wallet = useWalletStore()
 
@@ -13,14 +12,7 @@ const { clrfund, clrfunds } = storeToRefs(app)
 const { isConnected, account, chainId } = storeToRefs(wallet)
 
 watch([isConnected, account, chainId], async () => {
-  if (!isConnected.value) {
-    clrfunds.value = []
-    app.resetApp()
-    return
-  }
-
-  clrfunds.value = await app.getClrFunds()
-
-  if (!clrfunds.value.includes(clrfund.value)) app.resetApp()
+  clrfunds.value = []
+  app.resetApp()
 })
 </script>
